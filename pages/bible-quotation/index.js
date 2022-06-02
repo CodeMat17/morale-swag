59057c40c86963f9a8a20522e1e48de28217ff0e
Matthew <matgold17@gmail.com>
2022-06-02 23:37:13 +0100

    updated

--- pages/bible-quotation/index.js
+++ pages/bible-quotation/index.js
@@ -1,12 +1,17 @@
 import React from 'react'
+import NavHeader from '../../components/nav/NavHeader';
 
 function BiblePage
 () {
   return (
-    <div>BiblePage
-        
+    <>
+      <NavHeader />
+     <div className='tracking-widest flex justify-center pt-32 font-bold text-2xl text-gray-300'>
+      Coming Soon
     </div>
-  )
+    </>
+   
+  );
 }
 
 export default BiblePage
--- pages/proverbs/index.js
+++ pages/proverbs/index.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import NavHeader from '../../components/nav/NavHeader';
 
 function ProverbPage() {
   return (
-    <div>ProverbPage</div>
-  )
+    <>
+      <NavHeader />
+      <div className='tracking-widest flex justify-center pt-32 font-bold text-2xl text-gray-300'>
+        Coming Soon
+      </div>
+    </>
+  );
 }
 
-export default ProverbPage
+export default ProverbPage;
