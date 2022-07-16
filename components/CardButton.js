ef83c7403cfb8ee1d7a05d237ed67f7e84fecc37
Matthew <matgold17@gmail.com>
2022-07-16 01:34:22 +0100

    updated

--- components/CardButton.js
+++ components/CardButton.js
@@ -3,7 +3,8 @@ import { faArrowRotateBack, faArrowRotateRight, faSpinner } from '@fortawesome/f
 
 function CardButton({ handleQuotes, isLoading }) {
   return (
-    <div>
+    <div className='flex space-x-6'>
+      {isLoading && (<p className='pt-6 text-sm text-red-400 tracking-widest'>loading...</p>)}
       <button
         onClick={handleQuotes}
         className='flex items-center justify-center group bg-lime-700 border-2 border-lime-500 p-2 rounded-full text-green-900 hover:shadow-xl hover:shadow-emerald-600 hover:scale-105 transition duration-300 ease-in'>
